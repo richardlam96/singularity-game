b199e42aa75d41649738d4e9cd9e694b0278d127
Richard Lam <richardlam96@gmail.com>
2022-03-04 16:48:39 -0500

    Moved game init items to index to not clutter up game.js.
    Decided to stay with dependency injection into the game class so I can
    use it as an object that stores things I'll need throughout the rest of
    the code. Name can be changed later.

--- src/game.js
+++ src/game.js
@@ -1,63 +1,12 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { AssetFactory } from "./utilities/asset-factory";
-import { RandomGenerator } from "./utilities/random-generator";
 
 export class Game {
-    constructor() {
-        this._assetFactory = new AssetFactory();
-        this._renderer = new THREE.WebGLRenderer({antialias: true});
-        this._scene = new THREE.Scene();
-        this._camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
-        this._init();
-    }
-
-    _init() {
-        // Init Renderer.
-        this._renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this._renderer.domElement);
-
-        // Init Camera Controls.
-        this._camera.position.set(0, 10, 0);
-        this._controls.update();
-        this._camera.lookAt(0, 0, 0);
-
-        // Add a Grid for easier visibility and positioning.
-        let grid = new THREE.GridHelper(50, 50);
-        this._scene.add(grid);
-
-        // Add a light from the top.
-        const directionalLight = new THREE.DirectionalLight(0xffffff);
-        directionalLight.castShadow = true;
-        this._scene.add(directionalLight);
-    }
-
-    loadObjects() {
-        this._assetFactory
-        .init()
-        .then(() => {
-            let plane = this._assetFactory.getPlane();
-            plane.position.set(0, 0, 0);
-            this._scene.add(plane);
-
-            for (let i = 0; i < 10; i++) {
-                let cube = this._assetFactory.getCube();
-                let x = RandomGenerator.randIntBetween(0, 20);
-                let z = RandomGenerator.randIntBetween(0, 20);
-                cube.position.set(x, 0, z);
-                this._scene.add(cube);
-            }
-        });
+    constructor(scene, camera, light) {
+        this.scene = scene;
+        this.camera = camera;
+        this.light = light;
     }
 
     update() {
-        this._controls.update();
-    }
-
-    render = () => {
-        requestAnimationFrame(this.render);
-        this.update();
-        this._renderer.render(this._scene, this._camera);
     }
 }
