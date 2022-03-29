17872ed636c669c1d00ab36561c12dc25ba9933c
Richard Lam <richardlam96@gmail.com>
2022-03-29 12:42:10 -0400

    Added a quick class with RPGStats for objects and player.
    Stats included hp, poise, and speed basically, which will be used to
    calculate things that are seen in game.

---
+++ src/game-objects/rpg-stats.js
@@ -0,0 +1,17 @@
+export class RPGStats { 
+    constructor(params) {
+        this.hp = params.hp;
+        this.poise = params.poise;
+    }
+}
+
+export class PlayerRPGStats extends RPGStats {
+    constructor(params) {
+        super(params);
+        this.speed = params.speed;
+        this.turnSpeed = params.turnSpeed;
+        this.missileDelay = params.missileDelay;
+        this.missileDamage = params.missileDamage;
+        this.missileSpeed = params.missileSpeed;
+    }
+}
